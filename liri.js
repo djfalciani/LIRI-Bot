140b47b0cf277ba9a2b9508ea2bdd1ed3c77a5d4
djfalciani <dfalciani@gmail.com>
2019-07-28 00:31:40 -0400

    Added Spotify this Song Method

--- liri.js
+++ liri.js
@@ -1,22 +1,42 @@
 require("dotenv").config();
-var moment = require('moment');
-const keys = require("./keys.js");
+var ConcertEvent = require("./BandsInTown.js");
+var Spotify = require('node-spotify-api');
 
-function Spotify(obj) {
-    this.id = obj.id;
-    this.secret = obj.secret;
-}
+let command = process.argv[2];
+let search = process.argv.slice(3).join(" ");
 
-const spotify = new Spotify(keys.spotify);
-// console.log(spotify);
-// console.log("Hello World");
-// console.log(process.env.SPOTIFY_ID)
-// var test = keys.spotify.id;
-// console.log(test);
+// Create a new TV object
+var concert = new ConcertEvent();
+
+var spotify = new Spotify({
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+  });
 
 // Commands: concert-this, spotify-this song, movie-this, do-what-it-says
-
-let date = moment().format('dddd');
-let date2 = moment().format('L');
-console.log(date);
-console.log(date2);
+if (command === "concert-this") {
+    concert.findConcert(search);
+} else if (command === 'spotify-this-song') {
+    // By default, if no search term is provided, search for "Andy Griffith"
+    if (!search) {
+        search = "The Sign";
+    }
+    spotify.search({
+        type: 'track', query: `${search}`
+    }).then(function(response) {
+        const jsonData = response.tracks.items;
+        // console.log(jsonData[0].artists[0].name);
+        // console.log(jsonData[0].name);
+        // console.log(jsonData[0].preview_url);
+        // console.log(jsonData[0].album.name);
+    
+        jsonData.forEach(element => {
+            const divider = `\n-----------------------------\n`;
+            let SongData2 = `${divider}Artist(s): ${element.artists[0].name} \nSong: ${element.name} \nPreview URL: ${element.preview_url} \nAlbum: ${element.album.name} ${divider}`;
+            console.log(SongData2);
+        });
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+} 
